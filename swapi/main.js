af474685ed670e184e226754845cc64579b53b63
Mason Maag <Masonmaag171@gmail.com>
2022-05-25 15:40:37 -0600

    initial commit

---
+++ swapi/main.js
@@ -0,0 +1,27 @@
+const residentsBtn = document.querySelector("#residents")
+
+const buttonClick = evt => {
+    evt.preventDefault()
+ 
+    axios
+        .get("https://swapi.dev/api/planets/2/")
+        .then(response => {
+            console.log(response.data)
+            const { residents } = response.data
+            residents.forEach(el => {
+                axios
+                    .get(el)
+                    .then(res => {
+                    console.log(res.data)
+                    const { name } = res.data
+                    let nameTag = document.createElement("h2")
+                    nameTag.textContent = name
+                    document.body.appendChild(nameTag)       
+                    })
+                    .catch(err => console.log(err))
+            })
+        })
+        .catch(err => console.log(err))
+}
+
+residentsBtn.addEventListener('click', buttonClick)
